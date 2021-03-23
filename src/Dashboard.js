463b363b89993a3ec08ba66efefa48f99fcec338
Hrishika Saxena <hrishika16saxena@gmail.com>
2021-03-23 16:56:13 +0530

    dashboard

---
+++ src/Dashboard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './css/dashboard.css'
+import Sidebar from './Sidebar'
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div className='contt'>
+                <Sidebar/>
+                <div className='main'>
+                    saxena
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Dashboard
