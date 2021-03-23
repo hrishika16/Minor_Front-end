a9c22bfb375318a97e5a75c96c6d4ee7dc240a37
Hrishika Saxena <hrishika16saxena@gmail.com>
2021-03-23 17:00:04 +0530

    header section

--- src/Admin/Header.js
+++ src/Admin/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg nav_d">
+                <nav className="navbar navbar-expand-lg nav_d ">
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
