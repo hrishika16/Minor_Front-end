fdb163c5a6c87a9ae383cacdc11749794654c97b
Hrishika Saxena <hrishika16saxena@gmail.com>
2021-04-17 18:35:03 +0530

    connections page

---
+++ src/Connections.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import './css/connection.css'
+import HeaderUser from './HeaderUser'
+import axios from 'axios'
+import userI from './img/userIcon.svg'
+
+class Connections extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            connections : [],
+            connections_half : [],
+            connections_anotherhalf : []
+        }
+    }
+
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(resp =>{
+            console.log(resp.data)
+            this.setState({
+                connections : resp.data,
+                connections_half : this.state.connections.slice(0,7),
+                connections_anotherhalf : this.state.connections.slice(8,14)
+            })
+        })
+        .catch(error =>{
+            console.log('err',error)
+        })
+    }
+
+    renderConnectionsInfo(){
+        return this.state.connections.map((user) => {
+            return (
+                <React.Fragment key={user.id}>
+                    <div className='box_13'>
+                        <div className='row'>
+                            <div className='col-lg-3'>
+                                <img src={userI} alt='user Icon' className='user_rImg' />
+                            </div>
+                            <div className='col-lg-9 padr_dd'>
+                                <p className='proff'>{user.username}</p>
+                                <p className='profeS'>{user.email}</p>
+                                <button className='unfllow_btn'>Unfollow</button>
+                            </div>
+                        </div>
+                    </div>    
+               </React.Fragment>
+            )
+         })
+    }
+    
+
+    render() {
+        return (
+            <div className='whole_25'>
+                <HeaderUser/>
+                <div className='conn_12'>
+                    <div>
+                        <p className='text-center connectio'>CONNECTIONS</p>
+                    </div>
+                    <div className='container'>
+                        <div className='row'>
+                            <div className='col-lg-6 col-12'>
+                                {this.renderConnectionsInfo()}
+                            </div>
+                            <div className='col-lg-6 col-12'>
+                                {this.renderConnectionsInfo()}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Connections
