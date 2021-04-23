0ec11a56415ee3162792939db21ff052dab904ed
kalakhushi18 <khushikala18@gmail.com>
2021-04-23 18:46:27 +0530

    changes in contactUs

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -7,15 +7,13 @@ function ContactUs() {
         
 <div className="container-fluid complete" >
     <div className="row" >
-      <div className="col col-lg-6 columnFirst">
+      <div className="col-12 col-lg-6 columnFirst">
         <h1 className="heading">Contact Us</h1>
-        <h2>Get In Touch With Us</h2>
-        <h3>Click on the below button to post your queries.</h3>
-        
-        <h3>We are here to help you out. </h3>
+        <h2 className="headingSecond">Get In Touch With Us</h2>
+        <h3 className="headingThird">Click on the below button to post your queries.<br></br>We are here to help you out.</h3>
         <button className="contactBtn">Contact Us </button>
-        </div>
-     <div className="col col-lg-6 columnSecond">
+      </div>
+     <div className="col-12 col-lg-6 columnSecond">
      <img src={contactImage} alt="contact us " width='100%' ></img>
       </div>
     </div>
