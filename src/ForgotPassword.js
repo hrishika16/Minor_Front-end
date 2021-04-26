132cdc83396ddd32951abad6f08f8de0aed063a9
kalakhushi18 <khushikala18@gmail.com>
2021-04-26 21:31:29 +0530

    Forgot password component

--- src/ForgotPassword.js
+++ src/ForgotPassword.js
@@ -1,19 +1,23 @@
 import React from 'react'
+import forgotPassword from './img/forgotPassword.jpg'
+import './css/forgotPassword.css'
 
 function ForgotPassword() {
     return (
         <div>
-           <div className="container">
+           <div className="container mainforgot">
                <div className="row">
-                   <div className="col-12 col-lg-6">
-                     <img src='' alt="forgot password"></img>
+                   <div className="col-12 col-lg-6 col-sm-12 ">
+                     <img src={forgotPassword} alt="forgot password" width="100%"></img>
                    </div>
-                   <div className="col-12 col-lg-6">
-                       <h1>Forgot Your Password ?</h1>
+                   <div className="col-12 col-lg-6 col-sm-12 forgotpswd">
+                       <h1>Forgot <br></br> Your Password ?</h1>
                        <form>
-                           <input type = "email" placeholder="Enter your Email-Id" id="email" name="email" ></input>
+                           <input type = "email" placeholder="Enter your Email-Id" id="email" name="email" className="inputforgotemail"></input>
                            <br></br>
-                           <button>Send Email</button>
+                           <button className="forgotSendBtn">Send Email</button>
+                           <br></br>
+                           <button className="forgotLoginBtn">Back to Login</button>
                        </form>
                    </div>
                </div>
