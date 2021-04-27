eec9b09c1387ae19d1e21a9b38f2f083d382a389
kalakhushi18 <khushikala18@gmail.com>
2021-04-28 00:35:08 +0530

    Mentor Post added

---
+++ src/Admin/MentorPost.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Sidebar from '../Sidebar'
+import Header from './Header'
+
+function MentorPost() {
+    return (
+        <div>
+            <Header></Header>
+            <Sidebar></Sidebar>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default MentorPost
