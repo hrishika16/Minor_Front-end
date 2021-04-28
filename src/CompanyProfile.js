d897dbd20045a7e7188b501d0f6427e5395c4bf9
kalakhushi18 <khushikala18@gmail.com>
2021-04-28 19:38:41 +0530

    company profile

--- src/CompanyProfile.js
+++ src/CompanyProfile.js
@@ -2,13 +2,15 @@ import React from 'react'
 import Header from './Admin/Header'
 import HeaderUser from './HeaderUser'
 import userIcon from './img/userIcon.svg'
+import './css/companyProfile.css'
+import building from './img/companyBuilding.jpg'
 
 function CompanyProfile() {
     return (
         <div>
-           <HeaderUser></HeaderUser>
-           <div className='cside'>
-                <div className='sidebard'> 
+           {/* <HeaderUser></HeaderUser> */}
+           <div className='mainCP'>
+                <div className='sidebardCP'> 
                     {/* User icon */}
                     <div>
                         <center>
@@ -16,6 +18,7 @@ function CompanyProfile() {
                         </center>
                     </div>
                     <br/>
+                    <br></br>
                     <div className='sidess_11'>
                         <button className='linkss_11'>Profile</button>
                     </div>
@@ -30,6 +33,23 @@ function CompanyProfile() {
                         <button className='linkss_11'>Your Post</button>
                     </div>
                 </div>
+            </div>
+            <div className = "container completeCP">
+              <div className = "row">
+              
+               <div className = "col-12 col-lg-3">
+                   <img src = {building} alt ="building" className = "imgCP"></img>
+               </div>
+               <div className = "col-12 col-lg-9 detailCP">
+               <h1 >Company Name </h1>
+               <br></br>
+               <h3> Email Company </h3>
+               <br></br>
+               <h4> Since {'$'}Year of Establishment </h4>
+                </div>
+                
+              </div>
+
             </div>
 
         </div>
