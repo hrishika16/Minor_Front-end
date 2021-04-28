367b4b6b38795b79533afcc7c3125287ea6445bd
kalakhushi18 <khushikala18@gmail.com>
2021-04-28 16:17:29 +0530

    company login changes

--- src/CompanyLogin.js
+++ src/CompanyLogin.js
@@ -1,23 +1,25 @@
 import React,{useState} from 'react'
 import axios from 'axios'
+import companyLogin from './img/companyL.png'
+import './css/companyLogin.css'
 
 function CompanyLogin() {
-    const[username , setUsername] = useState('')
+    const[email , setEmail] = useState('')
     const[password , setPassword] = useState('')
     const [login,setLogin] = useState(false)
 
     const SubmitHandler = (e) => {
         e.preventDefault()
       
-        if(username === ''){
-            document.querySelector('#userErr').innerHTML = 'Email is Required';
-            document.getElementById('error_1').style.display = 'block';
-            document.querySelector('#user-name').focus()
+        if(email === ''){
+          
+            document.getElementById('userErr').style.display = 'block';
+            // document.querySelector('#user-name').focus()
         }
         if(password === ''){
-        document.querySelector('#passErr').innerHTML = 'Password is Required';
-        document.getElementById('error_2').style.display = 'block';
-        document.querySelector('#password').focus()
+        // document.querySelector('#passErr').innerHTML = 'Password is Required';
+        document.getElementById('passErr').style.display = 'block';
+        // document.querySelector('#password').focus()
         }
        
         else {
@@ -63,25 +65,26 @@ function CompanyLogin() {
     }
     return (
         <div>
+        <div className = "companyLogin">
             <div className='ro'>
             <div className='row'>
                 <div className='col-lg-5 col-12'>
-                    {/* <img src={img1} alt='Login image 1' className='img1' /> */}
+                    <img src={companyLogin} alt='Login image 1' className='img1' />
                 </div>
                 <div className='col-lg-7 col-12'>
                     
                     <div className='spaceee'>
-                    <h2 className='sign'>Sign Up</h2>
+                    <h2 className='sign'>Login In</h2>
                     <div className='hgt1'>
-                        <p className='lbl_txt'> Username</p>
+                        <p className='lbl_txt'> Email </p>
                         <input 
-                        type="text" 
+                        type="email" 
                         className="form-control inp_login" 
-                        placeholder='Type here...'
+                        placeholder='Email-Id'
                         id='user-name' 
-                        value={username} 
-                        onChange={e=>setUsername(e.target.value)}/>
-                        <p className='error' id='userErr' ></p>   
+                        value={email} 
+                        onChange={e=>setEmail(e.target.value)}/>
+                        <p className='error' id='userErr' > Email is Required</p>   
                     </div>
                      <div className='hgt1'>
                         <p className='lbl_txt'>Password</p>
@@ -92,16 +95,15 @@ function CompanyLogin() {
                             id='password' 
                             value={password} 
                             onChange={e=>setPassword(e.target.value)}/>   
-                        <p className='error' id='passErr'></p>  
+                        <p className='error' id='passErr'>Password is Required</p>  
                      </div>
-                       
-                    <button className="btn_submit" onClick={SubmitHandler}>Login</button>
-                    {/* <div>
-                        or
-                    </div> */}
+                       <center>
+                    <button className="btn_submitCL" onClick={SubmitHandler}>Login</button>
+                    </center>
                     </div>
                 </div>
             </div>
+        </div>
         </div>
     )
         </div>
