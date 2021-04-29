f8df4fd21061a48352d82b34802dac2be7a3bf46
Hrishika Saxena <hrishika16saxena@gmail.com>
2021-04-29 10:41:50 +0530

    linking

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,6 +4,7 @@ import HeaderUser from './HeaderUser'
 import Sidebar from './Sidebar'
 import axios from 'axios'
 import userI from './img/userIcon.svg'
+import userIcon from './img/userIcon.svg'
 
 class Dashboard extends Component {
     constructor(props) {
@@ -11,7 +12,7 @@ class Dashboard extends Component {
     
         this.state = {
              requests : [],
-             request5 : []
+             request : []
         }
     }
 
@@ -20,9 +21,10 @@ class Dashboard extends Component {
         .then(resp =>{
             console.log(resp.data)
             this.setState({
-                requests : resp.data,
-                request5 : this.state.requests.slice(0,7) 
+                requests : resp.data.slice(0,7) ,
+                request : this.state.requests.slice(0,7) 
             })
+            console.log(this.state.requests)
         })
         .catch(error =>{
             console.log('err',error)
@@ -30,7 +32,7 @@ class Dashboard extends Component {
     }
 
     renderRequests(){
-        return this.state.request5.map((user) => {
+        return this.state.requests.map((user) => {
             return (
                 <React.Fragment key={user.id}>
                 <div className='box_11'>
@@ -41,6 +43,8 @@ class Dashboard extends Component {
                         <div className='col-lg-9 padr_dd'>
                             <p className='userName'>{user.username}</p>
                             <p className='profeSS'>{user.email}</p>
+                            {/* <p className='userName'>Username</p>
+                            <p className='profeSS'>profession</p> */}
                         </div>
                     </div>
                 </div>    
@@ -63,6 +67,20 @@ class Dashboard extends Component {
                                 <div className='col-md-8'>
                                     <div className='posts'>
                                         <p className='had_txt'>Posts</p>
+                                        <center>
+                                        <div className='post_box'>
+                                            <div className='row'>
+                                                <div className='col-3'>
+                                                    <img src={userIcon} alt='profile' className='post_Img' />
+                                                </div>
+                                                <div className='col-9'>
+                                                    <p className='user_Name'>Name</p>
+                                                </div>
+                                            </div>
+                                            <div className='box_postt'></div>
+                                            <button className='star_btn'>Star</button> <button className='count_btn'>Count</button>
+                                        </div>
+                                    </center>
                                     </div>
                                 </div>
                                 <div className='col-md-4'>
