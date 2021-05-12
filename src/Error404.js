53ffd7f37ffba45fc104ae62333061f23ee8ea5f
kalakhushi18 <khushikala18@gmail.com>
2021-05-12 17:15:14 +0530

    homepage redirection

--- src/Error404.js
+++ src/Error404.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import error_page from './img/error404.jpg'
 import './css/error.css'
+import { Link } from 'react-router-dom'
 
 function Error404() {
+ 
+  
+
     return (
         <div>
         <div className="container errorMessage">
         <center>
          <img src ={error_page} alt="error 404" width ="100%" ></img> 
        
-        <button className="homepageBtn">Back to Homepage</button>
+        <button className="homepageBtn"><Link to ='/'  style={{ textDecoration: 'none' ,color : '#fff' }}>Back to Homepage </Link></button>
         </center>
         </div>
        
