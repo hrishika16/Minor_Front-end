a55e23aaad27439919cbb0da3c1f989b5230695f
kalakhushi18 <khushikala18@gmail.com>
2021-05-12 16:04:20 +0530

    help component added

---
+++ src/Help.js
@@ -0,0 +1,152 @@
+import React from 'react'
+import logout from './img/logout.svg';
+import syncIn from './img/logo.png'
+import './css/help.css'
+
+function Help() {
+    return (
+        <div>
+       <div className ="tryyy">
+        <nav class="navbar navbar-expand-lg navbar-light">
+        <div className='logo class="navbar-brand"'>
+                     <img src= {syncIn} alt= "logo" className="logoImg"></img>
+      </div>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul className="navbar-nav mr-auto mt-2 mt-lg-0 ml-auto">
+    
+      <li className="nav-item">
+        <h3 className = "help_heading">Help</h3>
+      </li>
+    
+   
+    </ul>
+    <div class="form-inline my-2 my-lg-0">
+
+      <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Home </button>
+    </div>
+  </div>
+</nav>
+</div>   
+<br></br>
+<center>
+ <div >
+     <h2 className = "heading_other" >We Are Here To Help You </h2>
+ </div>
+ </center>
+ <br>
+
+ </br>
+ <div className = "container-fluid">
+     <div className = "row">
+         <div className = "col-lg-3">
+         <center>
+            <h3 className = "home_heading ">Home</h3>
+            <br></br>
+            <div className="dropright">
+           
+  <button className="btn btn-light dropdown-toggle btn_dropHelp " type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;
+  </button>
+  <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
+    <button className="dropdown-item" type="button">Action</button>
+    <button className="dropdown-item" type="button">Another action</button>
+    <button className="dropdown-item" type="button">Something else here</button>
+  </div>
+</div>
+ <div className="dropright">
+           
+           <button className="btn btn-light dropdown-toggle btn_dropHelp " type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+           Dropdown &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;
+           </button>
+           <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
+             <button className="dropdown-item" type="button">Action</button>
+             <button className="dropdown-item" type="button">Another action</button>
+             <button className="dropdown-item" type="button">Something else here</button>
+           </div>
+         </div>
+         <div className="dropright">
+           
+           <button className="btn btn-light dropdown-toggle btn_dropHelp" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+           Dropdown &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;
+           </button>
+           <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
+             <button className="dropdown-item" type="button">Action</button>
+             <button className="dropdown-item" type="button">Another action</button>
+             <button className="dropdown-item" type="button">Something else here</button>
+           </div>
+         </div>
+ </center>
+         </div>
+         <div className = "col-lg-9">
+             <h3 className = "home_heading">Popular Actions </h3>
+             <div className = 'container'>
+                 <div className = "row">
+                     <div className ="col-lg-6">
+                        <p className = "popular_actions">Lorem ipsum dolor sit amet, </p>
+                        <p className = "popular_actions">Lorem ipsum dolor sit amet, </p>
+                        <p className = "popular_actions"> Lorem ipsum dolor sit amet, </p>
+                     </div>
+                     <div className ="col-lg-6">
+                     <p className = "popular_actions">Lorem ipsum dolor sit amet, </p>
+                        <p className = "popular_actions">Lorem ipsum dolor sit amet, </p>
+                        <p className = "popular_actions">Lorem ipsum dolor sit amet, </p>
+                    </div>
+                 </div>
+             </div>
+             <br></br>
+             <div>
+                 <h3 className = "home_heading">Suggested For You</h3>
+                 <h4 className="heading_suggested">Lorem ipsum</h4>
+                 <p className = "popular_actions">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                
+                 <hr></hr>
+                 <br></br>
+                 <h4 className="heading_suggested">Lorem ipsum</h4>
+                 <p className = "popular_actions">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+             
+              <hr></hr>
+              <br></br>
+              <h4 className="heading_suggested">Lorem ipsum</h4>
+                 <p className = "popular_actions">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+
+<hr></hr>
+              <br></br>
+              <h4 className="heading_suggested">Lorem ipsum</h4>
+                 <p className = "popular_actions">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+<hr></hr>
+
+              <br></br>
+              <h4 className="heading_suggested">Lorem ipsum</h4>
+                 <p className = "popular_actions">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                 <hr></hr>
+
+             </div>
+             <br></br>
+             <div >
+     <h3 className="home_heading">
+         Safety Measures 
+     </h3>
+     <p className = "popular_actions">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor iLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor iLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i</p>
+ </div>
+         </div>
+     </div>
+     <hr></hr>
+     <div class="footer-copyright text-center py-3 footcp">© 2021 Copyright:
+    <a href="#">SyncIn.com</a>
+  </div>
+     
+ </div>
+
+
+    </div>
+
+    )
+}
+
+export default Help
