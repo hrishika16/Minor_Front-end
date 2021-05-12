ae3dadbd51589ab61796995f2c7ce140c979870b
kalakhushi18 <khushikala18@gmail.com>
2021-05-12 16:04:32 +0530

    privacy added

---
+++ src/Privacy.js
@@ -0,0 +1,108 @@
+import React from 'react'
+import syncIn from './img/logo.png'
+import './css/privacy.css'
+
+
+function Privacy() {
+    return (
+        <div>
+               <div className ="tryyy">
+        <nav class="navbar navbar-expand-lg navbar-light">
+        <div className='logo class="navbar-brand"'>
+                     <img src= {syncIn} alt= "logo" className="logoImg"></img>
+      </div>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul className="navbar-nav mr-auto mt-2 mt-lg-0 ml-auto">
+    
+      <li className="nav-item">
+        <h3 className = "help_heading">Privacy Policy </h3>
+      </li>
+    
+   
+    </ul>
+ </div>
+</nav>
+</div>   
+<br></br>
+<div className = 'container'>
+
+<h2 className = "your_heading" >Your Privacy Matters</h2>
+<p className = "descrip">SyncIn mission is to connect the world’s professionals to allow them to be more productive and successful. Central to this mission is our commitment to be transparent about the data we collect about you, how it is used and with whom it is shared.
+
+This Privacy Policy applies when you use our Services (described below). We offer our users choices about the data we collect, use and share as described in this Privacy Policy, Settings and our Help Center.</p>
+
+<h4 className = "topic_heading"> Key Terms </h4>
+<br></br>
+<h5 className = "topic_heading">Table of Contents</h5>
+<a className = "descrip">1. Data We Collect</a>
+<br></br>
+<a className = "descrip">2. How We Use Your Data</a>
+<br></br>
+<a className = "descrip">3. How We Share Information</a>
+<br></br>
+<a className = "descrip">4. Your Choices and Obligations</a>
+<br></br>
+<a className = "descrip">5. Other Important Information</a>
+
+<br></br>
+<br></br>
+<h2 className = "topic_heading">Introduction</h2>
+<div class="alert alert-primary" role="alert">
+We are a social network and online platform for professionals. People use our Services to find and be found for business opportunities, to connect with others and find information. Our Privacy Policy applies to any Member or Visitor to our Services.
+</div>
+<p className = "descrip">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+<h3 className = "topic_heading">Services </h3>
+<div class="alert alert-primary" role="alert">
+We are a social network and online platform for professionals. People use our Services to find and be found for business opportunities, to connect with others and find information. Our Privacy Policy applies to any Member or Visitor to our Services.Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit
+</div>
+<p className = "descrip">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+<h3 className = "topic_heading">Change </h3>
+<div class="alert alert-primary" role="alert">
+We are a social network and online platform for professionals. People use our Services to find and be found for business opportunities, to connect with others and find information. Our Privacy Policy applies to any Member or Visitor to our Services.Lorem ipsum dolor sit amet, consectetur adipiscing elit
+</div>
+<p className = "descrip">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+<hr></hr>
+<h2 className = "topic_heading">Other Information</h2>
+<br></br>
+<br></br>
+<h3>Security  </h3>
+<div class="alert alert-secondary" role="alert">
+We are a social network and online platform for professionals. People use our Services to find and be found for business opportunities, to connect with others and find information. Our Privacy Policy applies to any Member or Visitor to our Services.Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit
+</div>
+<p className = "descrip">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+<h3 className = "topic_heading">Lawful Basis for Processing   </h3>
+<div class="alert alert-secondary" role="alert">
+We are a social network and online platform for professionals. People use our Services to find and be found for business opportunities, to connect with others and find information. Our Privacy Policy applies to any Member or Visitor to our Services.Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit
+</div>
+<p className = "descrip">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+<h3 className = "topic_heading">Direct Marketing   </h3>
+<div class="alert alert-secondary" role="alert">
+We are a social network and online platform for professionals. People use our Services to find and be found for business opportunities, to connect with others and find information. Our Privacy Policy applies to any Member or Visitor to our Services.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit
+</div>
+<p className = "descrip">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+<h3 className = "topic_heading">Contact Information   </h3>
+<div class="alert alert-secondary" role="alert">
+We are a social network and online platform for professionals. People use our Services to find and be found for business opportunities, to connect with others and find information. Our Privacy Policy applies to any Member or Visitor to our Services.Lorem ipsum dolor sit amet, consectetur adipiscing elit
+</div>
+<p className = "descrip">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+<hr></hr>
+
+</div>
+ <div class="footer-copyright text-center py-3 footcp">© 2021 Copyright:
+    <a href="#">SyncIn.com</a>
+  </div>
+        </div>
+    )
+}
+
+export default Privacy
