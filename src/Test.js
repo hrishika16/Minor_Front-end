dec08a99da4535b2e60b2d47adea69fcbecb0575
Hrishika Saxena <hrishika16saxena@gmail.com>
2021-05-13 19:07:18 +0530

    testing component

---
+++ src/Test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './css/test.css'
+
+function Test() {
+    return (
+        <div>
+            <div className="wrapper">
+            <div className="pie spinner"></div>
+            <div className="pie filler"></div>
+            <div className="mask"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Test
