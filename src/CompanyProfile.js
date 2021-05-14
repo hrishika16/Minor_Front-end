248b96370bef0a3e26c672db8c6cfba18904915a
Hrishika Saxena <hrishika16saxena@gmail.com>
2021-05-14 18:43:00 +0530

    changes in here

--- src/CompanyProfile.js
+++ src/CompanyProfile.js
@@ -1,10 +1,8 @@
 import React,{useState,useEffect} from 'react'
-import Header from './Admin/Header'
-import HeaderUser from './HeaderUser'
-import userIcon from './img/userIcon.svg'
 import './css/companyProfile.css'
 import axios from 'axios'
 import building from './img/companyBuilding.jpg'
+import CompanySidebar from './CompanySidebar'
 
 function CompanyProfile() {
 
@@ -17,76 +15,59 @@ function CompanyProfile() {
             url : 'http://localhost:3001/companyProfile',
             data: {
                 'companyid' : companyid
-              },
-              headers : {
+            },
+            headers : {
                 AuthKey : 'asdfgh '
-              }
+            }
         }) 
         // setdataPost([res.data])
         .then(resp=>{
             console.log(resp)
             if(resp.data.status === 200){
               //error occur 
-              setCompanyData([resp.data.data[0]])
-              console.log(resp.data.message)
+            setCompanyData([resp.data.data[0]])
+            console.log(resp.data.message)
             }
             else if(resp.data.status === 202) {
               // console.log(res.data.message);
-              console.log(" Please center with correct credentials")
+                console.log(" Please center with correct credentials")
             }
             else{
                 console.log("Some error occured")
             }
-          })
-          .catch(error=>{
+        })
+        .catch(error=>{
             console.log(error)
-          })
+        })
     },[])
+
+    const renderCompanyData = () =>{
+        return companyData.map((data) =>{
+            return (
+                <React.Fragment key={data.companyId}>
+                    <div className = "row">
+                        <div className = "col-12 col-lg-3">
+                            <img src = {building} alt ="building" className = "imgCP"></img>
+                        </div>
+                        <div className = "col-12 col-lg-9 detailCP">
+                            <h1 >{data.name} </h1>
+                            <br></br>
+                            <h4> {data.email} </h4>
+                            <br></br>
+                            <h4>     <a href="#" className="btn btn-secondary">Visit Website</a></h4>
+                        </div>
+                    </div>
+                </React.Fragment>
+            )
+        })
+    }
 
     return (
         <div>
-           <div className='mainCP'>
-                <div className='sidebardCP'> 
-                    {/* User icon */}
-                    <div>
-                        <center>
-                            <img src={userIcon} alt='user Icon' className='userImg' />
-                        </center>
-                    </div>
-                    <br/>
-                    <br></br>
-                    <div className='sidess_11'>
-                        <button className='linkss_11'>Profile</button>
-                    </div>
-                   
-                <div className='sidess_11'>
-                        <button className='linkss_11'>Jobs/InternShip</button>
-                    </div>
-                    <div className='sidess_11'>
-                        <button className='linkss_11'>Mentor Post </button>
-                    </div>
-                    <div className='sidess_11'>
-                        <button className='linkss_11'>Your Post</button>
-                    </div>
-                </div>
-            </div>
+            <div className='mainCP'>
+                <CompanySidebar/>
             <div className = "container completeCP">
-              <div className = "row">
-              
-               <div className = "col-12 col-lg-3">
-                   <img src = {building} alt ="building" className = "imgCP"></img>
-               </div>
-               <div className = "col-12 col-lg-9 detailCP">
-               <h1 >Company Name </h1>
-               <br></br>
-               <h4> Email Company </h4>
-               <br></br>
-               <h4>     <a href="#" class="btn btn-secondary">Visit Website</a></h4>
-                </div>
-                
-                
-              </div>
-
+                {renderCompanyData()}
             </div>
         
             <hr></hr>
@@ -96,12 +77,12 @@ function CompanyProfile() {
                
                    <div className = "col-6 col-lg-6 btnCPAlign">
                    <center>
-                   <button type="button" class="btn btn-info btnCP ">OverView </button>
+                   <button type="button" className="btn btn-info btnCP ">OverView </button>
                    </center>
                    </div>
                    <div className = "col-6 col-lg-6">
                    <center>
-                   <button type="button" class="btn btn-info btnCP">About Us </button>
+                   <button type="button" className="btn btn-info btnCP">About Us </button>
                    </center>
                 </div>
                       
@@ -111,15 +92,14 @@ function CompanyProfile() {
            <div className = "container">
                <div className = "row">
               
-                   <div className = "col-12 col-lg-12 cardCP"
-                   >
-                  <div class="card text-center">
-                    <div class="card-header bg-light text-info">
+                   <div className = "col-12 col-lg-12 cardCP">
+                  <div className="card text-center">
+                    <div className="card-header bg-light text-info">
                      OverView
                     </div>
-                    <div class="card-body cardCPW">
+                    <div className="card-body cardCPW">
                         
-                        <p class="card-text">W additional content.With supporting text below as a natural lead-in to additional content.With supporting text below as a natural lead-in to additional content.With supporting text below as a natural lead-in to additional content.With supporting text below as a natural lead-in to additional contenWith supporting text below as a natural lead-in to additional content.With supporting text below as a natural lead-in to additional contenWith supporting text below as a natural lead-in to additional content.With supporting text below as a natural lead-in to additional conten low as a natural lead-in to additional conten low as a natural lead-in to additional conten</p>
+                        <p className="card-text">W additional content.With supporting text below as a natural lead-in to additional content.With supporting text below as a natural lead-in to additional content.With supporting text below as a natural lead-in to additional content.With supporting text below as a natural lead-in to additional contenWith supporting text below as a natural lead-in to additional content.With supporting text below as a natural lead-in to additional contenWith supporting text below as a natural lead-in to additional content.With supporting text below as a natural lead-in to additional conten low as a natural lead-in to additional conten low as a natural lead-in to additional conten</p>
                    
                     </div>
                   
@@ -127,8 +107,8 @@ function CompanyProfile() {
                    </div>
                  
                </div>
-           </div>
-
+                </div>
+            </div>
         </div>
     )
 }
