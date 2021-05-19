ef702fe0658883d27a6e92291ed5ca070a254cba
kalakhushi18 <khushikala18@gmail.com>
2021-05-19 15:52:48 +0530

    hire details

---
+++ src/Admin/HireDetails.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from '../Sidebar'
+import Header from './MentorNavbar'
+
+function HireDetails() {
+    return (
+        <div>
+        <Header></Header>
+        <Sidebar></Sidebar>
+            <h1>Hiredetails</h1>
+        </div>
+    )
+}
+
+export default HireDetails
