e05315f0b958fd81c4781de84ea951aa3b855797
kalakhushi18 <khushikala18@gmail.com>
2021-05-22 14:58:15 +0530

    oops page added

--- src/oops.js
+++ src/oops.js
@@ -1,17 +1,23 @@
 import React from 'react'
-import ooops from './img/error404.jpg'
+import oops from './img/oops.jpg'
 import './css/error.css'
 import { Link } from 'react-router-dom'
 
-function Error404() {
+function Oops() {
  
   
 
     return (
         <div>
-        <div className="container errorMessage">
+        <div className="container">
         <center>
-         <img src ={oops} alt="error 404" width ="100%" ></img> 
+        <div className = "row">
+            <div className = "col-lg-12">
+            <h1 className = "nodata">No Data Found</h1>
+            <img src ={oops} alt="error 404" className = "oopsImg"></img> 
+            </div>
+        </div>
+      
        
         <button className="homepageBtn"><Link to ='/'  style={{ textDecoration: 'none' ,color : '#fff' }}>Back to Homepage </Link></button>
         </center>
@@ -21,4 +27,4 @@ function Error404() {
     )
 }
 
-export default Error404
+export default Oops
