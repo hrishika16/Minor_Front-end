bdd37df2d07586e0a38ceddfc601bfc2fec6b042
Hrishika Saxena <hrishika16saxena@gmail.com>
2021-06-19 15:12:30 +0530

    profile details improved

--- src/ProfileDetails.js
+++ src/ProfileDetails.js
@@ -4,12 +4,16 @@ import userIcon from './img/userIcon.svg'
 function ProfileDetails() {
     return (
         <div>
-            <button className='edit_btn_p' data-toggle="modal" data-target="#EditProfileModal">Edit profile</button>
-            <div className='row margin_11'>
-                <div className='col-lg-5'>
+            <div className='row'> 
+                <div className='col-12'> 
+                    <button className='edit_btn_p' data-toggle="modal" data-target="#EditProfileModal">Edit profile</button>
+                </div>
+            </div>
+            <div className='row '>
+                <div className='col-lg-5 col_right_pdn'>
                     <img src={userIcon} alt='user Icon' className='userImg1' />
                 </div>
-                <div className='col-lg-7'>
+                <div className='col-lg-7 col_left_pdn'>
                     <p className='name'>Name</p>
                     <button className='add_bio_btn'> ADD BIO </button>
                     <br/>
