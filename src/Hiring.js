445594bbcac08daee044b716a0af1e92d53f877f
Hrishika Saxena <hrishika16saxena@gmail.com>
2021-06-22 16:59:04 +0530

    connected with api

--- src/Hiring.js
+++ src/Hiring.js
@@ -1,4 +1,5 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
 import './css/hiring.css'
 import HeaderUser from './HeaderUser'
 import Sidebar from './Sidebar'
@@ -6,18 +7,79 @@ import hireimg from './img/weAreHiring.png'
 
 
 function Hiring() {
-  const[role, roleChange] = useState('');
-  const[industoryType , industoryTypeChange ] = useState('');
-  const[functionalArea , functionalAreaChange] = useState('');
-  const[employment ,employmentChange] = useState('')
-  const[roleCategory , roleCategoryChange] = useState('')
-  const[skills , skillsChange] = useState('')
-  //abhi by deafult daal rhi name 
-  const[companyName , companyNameChange] = useState("Company Name")
-  const[companyLoc , companyLocChange] = useState("Location")
-  const[description , descriptionChange] = useState('')
-  
+    // const[role, roleChange] = useState('');
+    // const[industoryType , industoryTypeChange ] = useState('');
+    // const[functionalArea , functionalAreaChange] = useState('');
+    // const[employment ,employmentChange] = useState('')
+    // const[roleCategory , roleCategoryChange] = useState('')
+    // const[skills , skillsChange] = useState('')
+    // //abhi by deafult daal rhi name 
+    // const[companyName , companyNameChange] = useState("Company Name")
+    // const[companyLoc , companyLocChange] = useState("Location")
+    // const[description , descriptionChange] = useState('')
 
+    const [datatoShow, setdatatoShow] = useState([])
+    const [dataofHireDetail, setdataofHireDetail] = useState([])
+
+
+    useEffect( ()=>{
+        const postId = localStorage.getItem('rememberMe')
+        axios({
+            method : 'post',
+            url : 'http://localhost:3001/companyHirePost',
+            data: {
+                'postId' : postId
+            },
+            headers : {
+                AuthKey : 'asdfgh'
+            }
+        }) 
+        .then(resp=>{
+            console.log(resp)
+            if(resp.data.status === 200){
+                setdatatoShow(resp.data.companydata)
+                setdataofHireDetail(resp.data.postData)
+            }
+            else if(resp.data.status === 202) {
+            console.log(resp.data.message);
+            }
+            else{
+            console.log(resp.data.message);
+            }
+        })
+        .catch(error=>{
+        console.log(error)
+        })
+
+        
+    },[])
+
+    const renderPostData = () =>{
+        return datatoShow.map(user =>{
+            return(
+                <div key={user.companyId}>
+                    <h2 >{user.name}</h2> 
+                    <h3>{user.email}</h3> 
+                </div>
+            )
+        })
+    }
+
+    const renderHireData = () =>{
+        return dataofHireDetail.map(user =>{
+            return(
+                <div key={user.postID}>
+                   <p>Role : &nbsp; {user.role} </p>
+                    <p>Industory Type : &nbsp; {user.industryType}</p>
+                    <p>Functional Area : &nbsp; {user.functionalArea} </p>
+                    <p>Employment : &nbsp;  {user.employmentType} </p>
+                    <p>Role Category : &nbsp; {user.roleCategory} </p>
+                    <p>Skills : &nbsp; {user.skills}</p>
+                    <p>Job Description : &nbsp; {user.description}</p>
+                </div>
+            )
+        })
+    }
 
     return (
         <div>
@@ -28,13 +90,8 @@ function Hiring() {
             <br></br>
             <div className = "container wholeHiring">
                 <div className = "row">
-             
                     <div className = "col-lg-7 headHiring">
-                    <br></br>
-                    <br></br>
-                        <h2 >{companyName}</h2>
-                       
-                        <h3>{companyLoc}</h3>
+                        {renderPostData()}
                         <button>Apply on Company Website</button>
                     </div>
                     <div className = "col-lg-5">
@@ -58,13 +115,7 @@ function Hiring() {
                          <div className = "row">
                          
                              <div className = "col-lg-12 roles">
-                                <p>Role : &nbsp; {role} </p>
-                                <p>Industory Type : &nbsp; {industoryType}</p>
-                                <p>Functional Area : &nbsp; {functionalArea} </p>
-                                <p>Employment : &nbsp;  {employment} </p>
-                                <p>Role Category : &nbsp; {roleCategory} </p>
-                                <p>Skills : &nbsp; {skills}</p>
-                                <p>Job Description : &nbsp; {description}</p>
+                                {renderHireData()}
                             </div>
                         
                          </div>
