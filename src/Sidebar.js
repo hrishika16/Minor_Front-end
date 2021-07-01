bbe82dd588082f6776997acbde6868b0c9b70412
Hrishika Saxena <hrishika16saxena@gmail.com>
2021-07-01 19:04:22 +0530

    phone sidebar component added

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,61 +1,109 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import './css/sidebar.css'
-import userIcon from './img/userIcon.svg'
-import syncIn from './img/logo.png'
-import userProf from './img/userProf.png'
-import chat from './img/chat.png'
-import story from './img/cardbox.png'
-import post from './img/post.png'
-import yourStroy from './img/yourStroy.png'
-import axios from 'axios'
-import notif from './img/notif2.png'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./css/sidebar.css";
+import userIcon from "./img/userIcon.svg";
+import syncIn from "./img/logo.png";
+import userProf from "./img/userProf.png";
+import chat from "./img/chat.png";
+import story from "./img/cardbox.png";
+import post from "./img/post.png";
+import yourStroy from "./img/yourStroy.png";
+import axios from "axios";
+import notif from "./img/notif2.png";
 
 function Sidebar() {
+	return (
+		<div className="cside">
+			<div className="sidebard">
+				{/* User icon */}
+				<div>
+					<center>
+						<img src={userIcon} alt="user Icon" className="userImg" />
+					</center>
+				</div>
+				<br />
+				<div className="sidess_11">
+					<img src={userProf} alt="user icon" className="side_img" />
+					<Link to="/aboutUser" className="linkss_12">
+						Profile
+					</Link>
+				</div>
+				<div className="sidess_11">
+					<img src={chat} alt="user icon" className="side_img" />
+					<button className="linkss_11">Chat Screen</button>
+				</div>
+				<div className="sidess_11">
+					<img src={story} alt="user icon" className="side_img" />
+					<Link to="/hiring" className="linkss_12">
+						Hiring
+					</Link>
+				</div>
+				<div className="sidess_11">
+					<img src={yourStroy} alt="user icon" className="side_img" />
+					{/* successStory */}
+					<Link to="/successStory" className="linkss_12">
+						Success Story
+					</Link>
+				</div>
+				<div className="sidess_11">
+					<img src={notif} className="side_img"></img>
+					<button className="linkss_11">Notifications</button>
+				</div>
+				<div className="sidess_11">
+					<button
+						className="addStory"
+						data-toggle="modal"
+						data-target="#exampleAddPost">
+						{" "}
+						+ Add Post{" "}
+					</button>
+				</div>
+			</div>
+			{/* Phone sidebar */}
+			<div className="sidebarpp">
+				<nav id="sidebarp" className="sidebarPhone">
+					{/* Profile */}
+					<div className="sidess_11">
+						<img src={userProf} alt="user icon" className="side_img" />
+						<Link to="/aboutUser" className="linkss_12">
+							Profile
+						</Link>
+					</div>
+					{/* legal updates */}
+					<div className="sidess_11">
+						<img src={chat} alt="user icon" className="side_img" />
+						<button className="linkss_11">Chat Screen</button>
+					</div>
+					<div className="sidess_11">
+						<img src={story} alt="user icon" className="side_img" />
+						<Link to="/hiring" className="linkss_12">
+							Hiring
+						</Link>
+					</div>
+					<div className="sidess_11">
+						<img src={yourStroy} alt="user icon" className="side_img" />
+						{/* successStory */}
+						<Link to="/successStory" className="linkss_12">
+							Success Story
+						</Link>
+					</div>
+					<div className="sidess_11">
+						<img src={notif} className="side_img"></img>
+						<button className="linkss_11">Notifications</button>
+					</div>
+					<div className="sidess_11">
+						<button
+							className="addStory"
+							data-toggle="modal"
+							data-target="#exampleAddPost">
+							{" "}
+							+ Add Post{" "}
+						</button>
+					</div>
+				</nav>
+			</div>
+		</div>
+	);
+}
 
-
-
-        return (
-            <div className='cside'>
-                <div className='sidebard'> 
-                    {/* User icon */}
-                    <div>
-                        <center>
-                            <img src={userIcon} alt='user Icon' className='userImg' />
-                        </center>
-                    </div>
-                    <br/>
-                    <div className='sidess_11'>
-                        {/* <button className='linkss_11'>Profile</button> */}
-                        <img src={userProf} alt='user icon' className='side_img' />
-                        <Link to='/aboutUser' className='linkss_12'>Profile</Link>
-                    </div>
-                    <div className='sidess_11'>
-                    <img src={chat} alt='user icon' className='side_img' />
-                        <button className='linkss_11'>Chat Screen</button>
-                    </div>
-                    <div className='sidess_11'>
-                    <img src={story} alt='user icon' className='side_img' /> 
-                        <Link to='/hiring' className='linkss_12'>Hiring</Link>
-                    </div>
-                    <div className='sidess_11'>
-                    <img src={yourStroy} alt='user icon' className='side_img' />
-                        {/* successStory */}
-                        <Link to='/successStory' className='linkss_12'>Success Story</Link>
-                    </div>
-                    <div className='sidess_11'>
-                    <img src={notif} className = "side_img"></img>
-                        <button className='linkss_11'>Notifications</button>
-                    </div>
-                    <div className='sidess_11'>
-                        <button className='addStory'  data-toggle="modal" data-target="#exampleAddPost"> + Add Post </button>
-                    </div>
-                </div>
-
-                
-            </div>
-        )
-    }
-
-
-export default Sidebar
+export default Sidebar;
