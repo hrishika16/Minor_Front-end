599b52b764de7e84ebcd1954cb5ac1efc8946963
Hrishika Saxena <hrishika16saxena@gmail.com>
2021-12-01 20:47:33 +0530

    about user's changes

--- src/UserAbout.js
+++ src/UserAbout.js
@@ -106,13 +106,13 @@ function UserAbout() {
             <div className='main_prof mm_11'>
                 <div className='container'>
                     <div className='row'>
-                        <div className='col-lg-7 col-12'>
+                        <div className='col-lg-7 col-md-6 col-12'>
                             <div className='about_prof'>
                                 {renderUserAbout()}
                             </div>
                         </div>
-                        <div className='col-lg-1 col-0'></div>
-                        <div className='col-lg-4 col-12 col_pdddd'>
+                        <div className='col-lg-1 col-1'></div>
+                        <div className='col-lg-4 col-md-5 col-12 col_pdddd'>
                             <div className='reqq'>
                                 <ProfileDetails/>
                             </div>
